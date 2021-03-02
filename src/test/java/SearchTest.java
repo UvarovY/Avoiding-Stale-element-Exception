5bfd38e2c7c3a55e732ab2ffa393ffedb8709b0f
yuvarov <yury.uvarov@dataart.com>
2021-03-02 22:38:42 +0200

    IEwebServer added

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -9,11 +10,20 @@ public class SearchTest {
     @Test
     public void openGoogleChromeTest(){
 
-        File file = new File("chromedriver.exe");
-        System.setProperty("webdriver.chromedriver",file.getAbsolutePath());
+        File file = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
         WebDriver driver = new ChromeDriver();
-        driver.get("google.com");
+        driver.get("https://youtube.com");
         System.out.println(driver.getTitle());
+
+        File file2 = new File("src/test/resources/IEDriverServer.exe");
+        System.setProperty("webdriver.ie.driver", file2.getAbsolutePath());
+        WebDriver driver1 = new InternetExplorerDriver();
+        driver1.get("https://google.com");
+        System.out.println(driver1.getTitle());
+
         driver.quit();
+        driver1.quit();
     }
 }
